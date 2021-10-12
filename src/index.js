6a0c10ecc4bf401f3396f6e330d16bf98172a17c
alexander16108 <alexandermayowa05@gmail.com>
2021-10-12 14:21:10 +0100

    writing function to populate list

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import './style.css';
+
+const container = document.getElementById('displayedScores');
+
+const Score = [
+  {
+    Name: 'martins law',
+    Score: 20,
+  },
+  {
+    Name: 'martins law',
+    Score: 20,
+  },
+  {
+    Name: 'martins law',
+    Score: 20,
+  },
+  {
+    Name: 'martins law',
+    Score: 20,
+  },
+];
+
+const populate = () => {
+  Score.forEach(element => {
+    const template = `
+    <li class='individualScores d-flex lst-sty'>
+    <p >${element.Name} : <span>${element.Score}</span></p>
+  </li>
+  <hr/>
+    `
+    container.innerHTML += template;
+  });
+}
+
+populate();
