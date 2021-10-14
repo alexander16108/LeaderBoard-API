b5add92c1a4322375f592434a302cfbffffc96b7
alexander16108 <alexandermayowa05@gmail.com>
2021-10-14 10:19:00 +0100

    updating index to host eventListeners functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import './style.css';
+import {getGame, refreshPage, submit} from './async.js'
 
 const displayScores = () => {
   const ul = document.getElementById('displayedScores');
   const li = document.createElement('li');
   ul.appendChild(li);
   
+  getGame();
+  refreshPage();
   
+   document.getElementById('submitScore').addEventListener('click', submit);
   
-   document.getElementById('submitScore').addEventListener('click');
-  
-  document.getElementById('refresh').addEventListener('click',);
+  document.getElementById('refresh').addEventListener('click', refreshPage);
 }
 
 document.addEventListener('DOMContentLoaded', displayScores);
